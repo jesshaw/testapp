ea5d71db067a5d066a3623f9e8b405756d7f62f7
mzxiao <mzxiao@ctrip.com>
2019-03-05 10:51:05 +0800

    must bolt on the validations property to the input of ComponentOptions and also noto the "output" of @Component in the form for VueClass<Vue> on windows platform

--- src/main/webapp/app/shims-vue.d.ts
+++ src/main/webapp/app/shims-vue.d.ts
@@ -1,4 +1,14 @@
 declare module '*.vue' {
   import Vue from 'vue';
+  // must bolt on the validations property to the input of ComponentOptions
+  // and also onto the "output" of @Component in the form of VueClass<Vue>
+  module 'vue/types/options' {
+    interface ComponentOptions<V extends vue> {
+      validations?: { [x: string]: any };
+    }
+    interface VueClass<V extends vue> {
+      validations?: { [x: string]: any };
+    }
+  }
   export default Vue;
 }
